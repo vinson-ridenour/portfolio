0aac16adb4defe3bce641906b84a05b039b868e5
Vinson Ridenour <vinson.ridenour@gmail.com>
2018-04-18 15:38:29 -0700

    fixes broken home page image

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,7 +34,7 @@
 		 * Background image
 		/* ---------------------------------------------- */
 
-		$('#intro').backstretch(['assets/images/_sunset.png']);
+		$('#intro').backstretch(['assets/images/sunset.png']);
 
 		/* ---------------------------------------------- /*
 		 * Navbar
